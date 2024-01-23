b8a727335cc76ea0c7fdb4a314930ca1b480a63c
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2024-01-22 18:56:06 -0600

    yes

---
+++ server.py
@@ -0,0 +1,44 @@
+# import the necessary packages
+from imutils import build_montages
+from datetime import datetime
+import numpy as np
+import imagezmq
+import argparse
+import imutils
+import cv2
+
+imageHub = imagezmq.ImageHub()
+
+# initialize the dictionary which will contain  information regarding
+# when a device was last active, then store the last time the check
+# was made was now
+lastActive = {}
+lastActiveCheck = datetime.now()
+# stores the estimated number of Pis, active checking period, and
+# calculates the duration seconds to wait before making a check to
+# see if a device was active
+ESTIMATED_NUM_PIS = 4
+ACTIVE_CHECK_PERIOD = 10
+ACTIVE_CHECK_SECONDS = ESTIMATED_NUM_PIS * ACTIVE_CHECK_PERIOD
+# assign montage width and height so we can view all incoming frames
+# in a single "dashboard"
+mW = 1
+mH = 1
+
+
+
+# start looping over all the frames
+while True:
+	# receive RPi name and frame from the RPi and acknowledge
+	# the receipt
+	(rpiName, frame) = imageHub.recv_image()
+	imageHub.send_reply(b'OK')
+	# if a device is not in the last active dictionary then it means
+	# that its a newly connected device
+	if rpiName not in lastActive.keys():
+		print("[INFO] receiving data from {}...".format(rpiName))
+		cv2.namedWindow(rpiName)
+	# record the last active time for the device from which we just
+	# received a frame
+	lastActive[rpiName] = datetime.now()
+	cv2.imshow(rpiName, frame)
